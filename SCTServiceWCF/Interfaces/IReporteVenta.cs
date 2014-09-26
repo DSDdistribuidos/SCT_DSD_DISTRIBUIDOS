15a9c248692558197db73612ed12d41bb71fbeda
lvalencia1286 <lvalencia1286@gmail.com>
2014-09-26 16:48:41 -0500

    Servicio de Reporte y Tarifa
    Se cargo el servicio de Reporte y Tarifa

---
+++ SCTServiceWCF/Interfaces/IReporteVenta.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using SCTServiceWCF.Dominio;
+
+namespace SCTServiceWCF.Interfaces
+{
+    [ServiceContract]
+    public interface IReporteVenta
+    {
+        #region Metodos para la Administrar un Tarifa
+        [OperationContract]
+        List<ReporteVenta> ListarReporteVentas();
+        #endregion
+    }
+}
