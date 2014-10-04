7e7f23f2ff9241131d040ddc1045cfeb18ec8aa5
4ngelc <ebangel@outlook.com>
2014-10-04 03:53:25 -0500

    Agregado Web Reference

--- WebAgencia/Centro.aspx.cs
+++ WebAgencia/Centro.aspx.cs
@@ -7,7 +7,7 @@ using System.Web.UI.WebControls;
 
 namespace WebAgencia
 {
-    public partial class Tarifa : System.Web.UI.Page
+    public partial class Centro : System.Web.UI.Page
     {
         protected void Page_Load(object sender, EventArgs e)
         {
